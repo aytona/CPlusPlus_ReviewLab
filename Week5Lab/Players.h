ac644c006ba6aee6fa8d9598a2e6956700a8be23
aytona <christopher.aytona@gmail.com>
2015-10-09 19:45:39 -0400

    Lab 5 added
    - TODO: Complete lab 5: only have improved Arrays.h from lab work

---
+++ Week5Lab/Players.h
@@ -0,0 +1,14 @@
+#ifndef _PLAYERS_H_
+#define _PLAYERS_H_
+
+template <typename T>
+class Player
+{
+private:
+	int memberID, health;
+
+public:
+
+};
+
+#endif 
---
+++ Week5Lab/Source.cpp
@@ -0,0 +1,10 @@
+#include "Arrays.h"
+#include "Players.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}

c369f2f37f30659e787934ff302bebbfc02d6f97
aytona <christopher.aytona@gmail.com>
2015-12-02 23:58:01 -0500

    Finals - Main structure (main.cpp + main.h setup)

---
+++ Final_Practice/Final_Practice/Final_Practice/Main.cpp
@@ -0,0 +1,55 @@
+#include "Main.h"
+
+int main()
+{
+	int input;
+	std::cout << "Finals Practice Program:\n"
+		<< "Stacks: 1\n"
+		<< "Queues: 2\n"
+		<< "Hash Tables: 3\n"
+		<< "Trees: 4\n"
+		<< "Advanced Sorting: 5\n"
+		<< "Heaps: 6\n"
+		<< "Graphs: 7\n";
+	do
+	{
+		std::cout << "Input test number: ";
+		std::cin >> input;
+		switch (input)
+		{
+		case 1:
+			StacksTest();
+			break;
+
+		case 2:
+			QueuesTest();
+			break;
+
+		case 3:
+			HashTablesTest();
+			break;
+
+		case 4:
+			TreesTest();
+			break;
+
+		case 5:
+			AdvancedSortingTest();
+			break;
+
+		case 6:
+			HeapsTest();
+			break;
+
+		case 7:
+			GraphsTest();
+			break;
+
+		default:
+			std::cout << "Invalid input!\n"
+				<< "Type -99 to end\n";
+			break;
+		}
+	} while (input != SENTINEL);
+	return 0;
+}
---
+++ Final_Practice/Final_Practice/Final_Practice/Main.h
@@ -0,0 +1,23 @@
+/*
+Finals are:
+- Everything from midterm
+- Stacks and Queues
+- Hash Tables
+- Trees
+- Advanced Sorting
+- Heaps
+- Graphs
+*/
+
+#pragma once
+
+#include <iostream>
+
+const int SENTINEL = -99;
+void StacksTest();
+void QueuesTest();
+void HashTablesTest();
+void TreesTest();
+void AdvancedSortingTest();
+void HeapsTest();
+void GraphsTest();
